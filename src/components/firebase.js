797bcbdb443d28fb0c4c7e4d3b8ba94584bb6dd5
spichap <quinteroaris844@gmail.com>
2024-08-05 17:30:02 -0600

    Update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/storage';
 
 const firebaseConfig = {
-   apiKey: "AIzaSyDmqx-ixwVMUveJXYUXp9EjEL-KnZVVYfU",
-  authDomain: "saturin-2df57.firebaseapp.com",
-  projectId: "saturin-2df57",
-  storageBucket: "saturin-2df57.appspot.com",
-  messagingSenderId: "170839087316",
-  appId: "1:170839087316:web:0af1b9a0929c6ee7b4c341"
+    apiKey: "AIzaSyAGQBOZsnd-x85AhTysaNgza9lanlhW4SA",
+  authDomain: "magia-infinita.firebaseapp.com",
+  projectId: "magia-infinita",
+  storageBucket: "magia-infinita.appspot.com",
+  messagingSenderId: "281230398674",
+  appId: "1:281230398674:web:377b8d9d8b1b29f62a490b"
 };
 
 firebase.initializeApp(firebaseConfig);
