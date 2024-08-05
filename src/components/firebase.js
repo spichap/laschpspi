8797d450d96944ef2968e7b37475b4830e182a21
spichap <quinteroaris844@gmail.com>
2024-08-05 02:24:20 -0600

    Update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC4qa-X9JIKcUqT1lk5eNkSjoGlv01YRF4",
-  authDomain: "imgsitas.firebaseapp.com",
-  projectId: "imgsitas",
-  storageBucket: "imgsitas.appspot.com",
-  messagingSenderId: "988788936396",
-  appId: "1:988788936396:web:40c1ba7700322a27033c69"
+   apiKey: "AIzaSyDmqx-ixwVMUveJXYUXp9EjEL-KnZVVYfU",
+  authDomain: "saturin-2df57.firebaseapp.com",
+  projectId: "saturin-2df57",
+  storageBucket: "saturin-2df57.appspot.com",
+  messagingSenderId: "170839087316",
+  appId: "1:170839087316:web:0af1b9a0929c6ee7b4c341"
 };
 
 firebase.initializeApp(firebaseConfig);
